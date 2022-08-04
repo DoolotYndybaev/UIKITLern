eb46c0c7be0004986eb1fd20794e222028610ddb
DoolotYndybaev <91968210+DoolotYndybaev@users.noreply.github.com>
2022-08-04 16:12:30 +0600

    Add UIAllertController

--- UIKITLern/TextFieldViewController.swift
+++ UIKITLern/TextFieldViewController.swift
@@ -8,23 +8,27 @@
 import UIKit
 
 class TextFieldViewController: UIViewController {
-
+    
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         label.textAlignment = .center
     }
-
+    
     @IBAction func donePressed(_ sender: UIButton) {
         
         guard textField.text?.isEmpty == false else { return }
-            
+        
         if let _ = Double(textField.text!) {
-            print("Name format is wrong")
+            let alert = UIAlertController(title: "Wrong format", message: "Please enter your name", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true)
         } else {
-        label.text = textField.text
+            label.text = textField.text
+            textField.text = nil
         }
     }
 }
