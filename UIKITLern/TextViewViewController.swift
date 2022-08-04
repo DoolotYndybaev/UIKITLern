09f95245367333e27cf9d6eb9203ee8051febe8c
DoolotYndybaev <91968210+DoolotYndybaev@users.noreply.github.com>
2022-08-04 22:20:40 +0600

    Добавили наблюдатели(NotificationCenter.default.addObserver) чтобы клавиатура не накладывался на наш текст . Т.е сейчас наш текст поднимается вверх когда поднимается клавиатура

--- UIKITLern/TextViewViewController.swift
+++ UIKITLern/TextViewViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class TextViewViewController: UIViewController {
-
+    
+    @IBOutlet weak var textViewBottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var countLabel: UILabel!
     @IBOutlet weak var textView: UITextView!
     
@@ -21,15 +22,41 @@ class TextViewViewController: UIViewController {
         textView.backgroundColor = self.view.backgroundColor
         textView.layer.cornerRadius = 10
         
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(updateTextView(notification:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(updateTextView(notification:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         self.view.endEditing(true) // Скрытие клавиатуры для любого обьекта
         
-//        textView.resignFirstResponder() // Скрытие клав-ы для конткретного обьекта
+        //        textView.resignFirstResponder() // Скрытие клав-ы для конткретного обьекта
     }
-
+    @objc func updateTextView(notification: Notification) {
+        guard let userInfo = notification.userInfo as? [String: Any],
+              let keyBoardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+        else { return }
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            textView.contentInset = UIEdgeInsets.zero
+        } else {
+            textView.contentInset = UIEdgeInsets(top: 0,
+                                                 left: 0,
+                                                 bottom: keyBoardFrame.height - textViewBottomConstraint.constant,
+                                                 right: 0)
+            textView.scrollIndicatorInsets = textView.contentInset
+        }
+        
+        textView.scrollRangeToVisible(textView.selectedRange)
+    }
+    
+    
 }
 extension TextViewViewController: UITextViewDelegate {
     
