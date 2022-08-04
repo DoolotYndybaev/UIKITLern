8f9917430382eec67272c13faf2ac9362f0283ab
DoolotYndybaev <91968210+DoolotYndybaev@users.noreply.github.com>
2022-08-04 15:26:32 +0600

    1)add sliderOutlet 2)set the value for label.text (String(slider.value)) 3) set a new properties for slider 4) add sliderAction where add logic for  changes self.view withAlphaCompnent

--- UIKITLern/UISegmentedController.swift
+++ UIKITLern/UISegmentedController.swift
@@ -11,16 +11,25 @@ class UISegmentedController: UIViewController {
     @IBOutlet weak var segmentedControll: UISegmentedControl!
     
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        slider.value = 1
 
-        label.isHidden = true
+        label.text = String(slider.value)
         label.font = label.font.withSize(35)
         label.textAlignment = .center
         label.numberOfLines = 2
         
         segmentedControll.insertSegment(withTitle: "third", at: 2, animated: true)
         
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .blue
     }
     @IBAction func choiceSegment(_ sender: UISegmentedControl) {
         label.isHidden = false
@@ -38,5 +47,12 @@ class UISegmentedController: UIViewController {
         default:
             print("Sothing is wrong")
         }
+    }
+    @IBAction func sliderAction(_ sender: UISlider) {
+        label.text = String(sender.value)
+        
+        let backGroundColor = self.view.backgroundColor
+        
+        self.view.backgroundColor = backGroundColor?.withAlphaComponent(CGFloat(sender.value))
     }
 }
